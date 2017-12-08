de61c03d006fd22361e874c40f407ca17714503e
Mikhail Shcherbakov <yuske.dev@gmail.com>
2017-12-09 00:31:34 +0300

    Add minimal repro for crashing on Win7.

--- Example/Program.cs
+++ Example/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Example
@@ -13,6 +14,11 @@ namespace Example
             //This is a bare bones example that shows the logic in which one might
             //Implement Spitfire into their application
             //TODO a full fledged example
+            Console.WriteLine("Create WebRtcSession111");
+            var test = new WebRtcSession("abc");
+            Thread.Sleep(60000);
+            Console.WriteLine(test.Id);
+            Console.ReadLine();
         }
     }
 }
